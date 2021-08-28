1a41607e1edd18ab0bf98af4867540cf60b5e2a8
shasank <shasankpudasaini@gmail.com>
2021-08-28 12:36:45 -0500

    chNged the url

--- src/main/java/com/example/myfirstspring/controller/PersonController.java
+++ src/main/java/com/example/myfirstspring/controller/PersonController.java
@@ -3,9 +3,10 @@ package com.example.myfirstspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 @Controller
-@RequestMapping("/person")
+@RequestMapping("/")
 public class PersonController {
 
     @GetMapping("/")
